d2fc251c9d49c6279e9abb5e1c715b00329231a9
priya <priya@railsfactory.org>
2011-03-11 14:34:26 +0530

    adding folders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GetMocha::Application.config.secret_token = '797093e232cdd88dc4f51592b0ce3c9bcead683c09060cb4bfda880ad603ff8e8349a66107c68c380c3ceece53bccd6144c0b106933bdb8431c31d5d6b58a270'
