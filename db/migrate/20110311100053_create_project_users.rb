d4c114cf07418a15a244e01d08746eeb1ea17df5
kiruba <kiruba.v@railsbuddies.com>
2011-03-11 16:00:54 +0530

    Project, project users, project guests migration added

---
+++ db/migrate/20110311100053_create_project_users.rb
@@ -0,0 +1,16 @@
+class CreateProjectUsers < ActiveRecord::Migration
+  def self.up
+    create_table :project_users do |t|
+      t.integer :user_id
+      t.integer :project_id
+      t.boolean :status
+      t.timestamps
+    end
+    add_index "project_users", :user_id
+    add_index "project_users", :project_id
+  end
+
+  def self.down
+    drop_table :project_users
+  end
+end
