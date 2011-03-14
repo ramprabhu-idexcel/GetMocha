51651e3932d96a7e9923eeff4908e33683d6d6f5
senthilkumar <senthilkumar@sedin.co.in>
2011-03-14 10:55:53 +0530

    relation to the user and the project added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,16 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable 
+         #~ :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   has_many :projects
+  has_one :project
   has_many :project_users
   has_many :guest_users
   has_many :chats
   has_many :messages
+  #~ has_many
 end
