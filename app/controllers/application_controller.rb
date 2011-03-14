ab36ffd52d7ed22cf94d5f22910c3db1b6c1969b
senthilkumar <senthilkumar@sedin.co.in>
2011-03-14 17:53:29 +0530

    custom layout used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :change_layout
+  
+  def change_layout
+    if devise_controller?
+      "before_login"
+    else
+      "application"
+    end
+  end
 end
