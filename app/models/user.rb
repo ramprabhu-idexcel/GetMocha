7bfb381022a09fb9f71b777d63b2296c51366735
senthilkumar <senthilkumar@sedin.co.in>
2011-03-14 14:16:08 +0530

    methods to find the starred messages added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
-<<<<<<< HEAD
          :recoverable, :rememberable, :validatable, :confirmable
          #~ :trackable,
-=======
-         :recoverable, :rememberable 
-         #~ :trackable, :validatable
->>>>>>> 42acfc72f650d3420a33e5778abc4907a625ac2a
-
+         
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   has_many :projects
@@ -20,7 +15,7 @@ class User < ActiveRecord::Base
   has_many :messages
   has_many :activities
   
-   #starred messages from all project
+  #starred messages from all project
   def starred_messages
     activities.find(:all,:conditions=>['resource_type=? AND is_starred=? AND is_delete=?',"Message",true,false])
   end
