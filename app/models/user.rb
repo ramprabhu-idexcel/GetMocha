875c47023d1001b74953c6b86b91323a82c68a47
senthilkumar <senthilkumar@sedin.co.in>
2011-03-14 13:39:28 +0530

    methods to find the starred message added

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,32 @@ class User < ActiveRecord::Base
   has_many :guest_users
   has_many :chats
   has_many :messages
-  #~ has_many
+  has_many :activities
+  
+   #starred messages from all project
+  def starred_messages
+    activities.find(:all,:conditions=>['resource_type=? AND is_starred=? AND is_delete=?',"Message",true,false])
+  end
+  
+  #starred messages from the project
+  def project_starred_messages(project_id)
+    all_starred_messages.collect{|a| a.resource.project_id==project_id}
+  end
+  
+  #starred count from all project
+  def starred_count
+    all_starred_messages.count
+  end
+  
+  #starred count of the individual project
+  def project_starred_count(project_id)
+    project_starred_messages(project_id).count
+  end
+  
+  #membership in the active projects
+  def active_projects
+    
+  end
+  
+  
 end
