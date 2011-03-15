58e761e23785097cda545d6c8204adeca457941e
Selva <c.selvakumar@sedin.co.in>
2011-03-15 18:59:24 +0530

    Custom email id remove part completed

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -43,6 +43,9 @@ class ProjectsController < ApplicationController
 		elsif params[:task_email]
 			@custom=CustomEmail.new(:custom_type=>"Task", :project_id=>@project.id, :email=>params[:task_email])
 			@custom.save
+		elsif params[:remove_email]
+			@custom=CustomEmail.find(params[:remove_email])
+			@custom.destroy
 		end
 			render :partial=>'settings_pane'
 	end
