e6c347261aa27b7724fdfb5dfcd678bd647eb3e4
senthilkumar <senthilkumar@sedin.co.in>
2011-03-15 14:39:35 +0530

    signup changed to jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,7 +2,20 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(document).ready(function() {
+
   $('.user_drop_down').click(function(){
     $('.account-dropdown').toggle();
   });
+  
+  if(typeof Signup!="undefined" && Signup==true)
+  {
+    $('#user_submit').click(function(){
+      $.ajax({
+        url:'/users',
+        data: $('form#user_new').serialize(),
+        type: "POST"        
+      });
+      return false;
+    });
+  }
 });
