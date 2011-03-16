dfc971e89a4aaa61d48a7235ae30691083a29734
kiruba <kiruba.v@railsbuddies.com>
2011-03-16 12:39:43 +0530

    Autocomplete for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,17 @@ class MessagesController < ApplicationController
 	#~ before_filter :authenticate_user!
 	layout 'application'
 	def new
-		
+		@a=User.find(:all,:select=>[:first_name,:email])
+		@b=Project.find(:all,:select=>[:name])
+		@tcMovies=[]
+		@Movies=[]
+		@a.each do |f|
+		@tcMovies<<"#{f.first_name}-'#{f.email}'"
 	end
+	@b.each do |g|
+		@Movies<<"#{g.name}"
+		end
+		end
 	def create
 		@message=Message.new(params[:message])
 		@message.user_id=current_user
