02c254501dded4f79a4023f9ebb937c787f3763b
kiruba <kiruba.v@railsbuddies.com>
2011-03-16 15:26:44 +0530

    activity added

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,10 +19,12 @@ class MessagesController < ApplicationController
 	def create
 		@a=User.find_by_email(params[:message][:recipient])
 		@b=Project.find_by_name(params[:message][:project])
-	
-    @message=Message.new(:subject=> "params[:message][:subject]", :message=> "params[:message][:message]",:user_id=> @a.id, :project_id=>@b.id)
-		
+	 p.current_user.id
+	 p"------------------"
+    @message=Message.new(:subject=> "params[:message][:subject]", :message=> "params[:message][:message]",:user_id=> current_user.id, :project_id=>@b.id)
 		@message.save
+		@activity=Activity.new(:resource_type=>"Message",:resource_id=>@message.id,:user_id=>@a.id,:is_subscribed=>true);
+		@activity.save
 		#~ @attach=Attachment.new(params[:data])
 		#~ @attach.message_id=@message.id
 		#~ @attach.save
