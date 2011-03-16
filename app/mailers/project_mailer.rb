e187d252e15cbea1b68b3db5aba1e214ac3c276d
Selva <c.selvakumar@sedin.co.in>
2011-03-16 18:19:05 +0530

    Method added for project completed in project mailer

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -1,10 +1,16 @@
 class ProjectMailer < ActionMailer::Base
   default :from => "from@example.com"
   
-def project_renamed(user,old_project,new_project,to_user)
+  def project_renamed(user,old_project,new_project,to_user)
+      @user = user
+      @old_project=old_project
+      @new_project=new_project
+      mail(:from=>"#{user.email}", :to=>"#{to_user.email}", :subject=>"#{old_project} Project Has Been Renamed to #{new_project}")
+  end
+  
+  def project_completed(project, user, to_user)
     @user = user
-    @old_project=old_project
-    @new_project=new_project
-    mail(:from=>"#{user.email}", :to=>"#{to_user.email}", :subject=>"#{old_project} Project Has Been Renamed to #{new_project}")
-end
+    @to_user = to_user
+    mail(:from=>"#{user.email}", :to=>"#{to_user.email}", :subject=>"#{projectname} has been completed")
+  end
 end
