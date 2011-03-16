df92518012ec147a07fca2c1a1d49ab80972ac93
kiruba <kiruba.v@railsbuddies.com>
2011-03-16 15:42:52 +0530

    commit after merge

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -20,13 +20,8 @@ class MessagesController < ApplicationController
 	def create
 		@a=User.find_by_email(params[:message][:recipient])
 		@b=Project.find_by_name(params[:message][:project])
-
-	
     @message=Message.new(:subject=> params[:message][:subject], :message=> params[:message][:message],:user_id=> @a.id, :project_id=>@b.id)
-		
 		@message.save
-		@activity=Activity.new(:resource_type=>"Message",:resource_id=>@message.id,:user_id=>@a.id,:is_subscribed=>true);
-		@activity.save
 		#~ @attach=Attachment.new(params[:data])
 		#~ @attach.message_id=@message.id
 		#~ @attach.save
