73c360f2d2103796639d54a7d845e5d37d11932d
Selva <c.selvakumar@sedin.co.in>
2011-03-16 20:23:49 +0530

    Validations done on messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -22,11 +22,22 @@ end
 	def create
 		@project=Project.find_by_name(params[:message][:project])
 		@message=Message.new(:subject=> params[:message][:subject], :message=> params[:message][:message],:user_id=>current_user.id, :project_id=>@project.id)
+			message=@message.valid?
+				errors=[]
+      @message.errors.each_full{|msg| 
+				errors<< msg 
+			} 
+		if message
 		@message.save
 		@to_users=params[:message][:recipient].split(', ')
 		Message.send_message_to_team_members(@project,@message,@to_users)
 		Message.send_notification_to_team_members(current_user,@to_users)
 		render :nothing=>true
+		else
+					render :update do |page|
+				page.alert errors.join("\n")
+			end
+		end
 	end
 	
 	
