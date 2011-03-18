16f700a45b4d03e5b20e47fa342622a1f32e9037
Selva <c.selvakumar@sedin.co.in>
2011-03-18 16:31:58 +0530

    Changes on attachment

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,6 +2,6 @@ class Attachment < ActiveRecord::Base
   belongs_to :attachable, :polymorphic => true	
   has_attachment :size => 1.megabyte..2.megabytes
   has_attachment :content_type => ['application/pdf', 'application/msword', 'text/plain']
-  has_attachment :storage => :file_system, :path_prefix => 'public/files'
+  has_attachment :storage => :file_system, :path_prefix => 'public/attachments'
   
 end
