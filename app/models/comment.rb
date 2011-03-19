5e8c0f221e9a2c61e0c77dd0106463d4ad18079c
senthilkumar <senthilkumar@sedin.co.in>
2011-03-19 18:42:58 +0530

    method to add activies added

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,15 @@ class Comment < ActiveRecord::Base
 	belongs_to :commentable, :polymorphic => true
 	belongs_to :user
 	belongs_to :attachable, :polymorphic => true
+  has_many :activities, :as => :resource, :dependent=>:destroy
+  after_create :add_in_activity
+  
+  def add_in_activity
+    self.commentable.project.users.each do |user|
+      activity=self.activities.create! :user=>user
+      activity.update_attribute(:is_read,true) if user.id==self.user_id
+    end
+  end
   
   def self.find_hash(id)
     comment=self.find_by_id(id,:select=>[:comment,:created_at,:user_id])
