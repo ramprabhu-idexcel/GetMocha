b86e7bb3e3017ce38147d05ff3b749daf1439c51
Selva <c.selvakumar@sedin.co.in>
2011-03-22 18:28:24 +0530

    modified in attachment.rb,edit.html

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,6 +3,4 @@ class Attachment < ActiveRecord::Base
   has_attachment :size => 1.megabyte..2.megabytes
   #~ has_attachment :content_type => ['application/pdf', 'application/msword', 'text/plain']
   has_attachment :storage => :file_system, :path_prefix => 'public/attachments'
-
-
-end
+ end
