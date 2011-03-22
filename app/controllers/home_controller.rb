e634ddd35c9d18595ff8010e37c53c671459c559
Selva <c.selvakumar@sedin.co.in>
2011-03-22 17:50:01 +0530

    Routes added for sendgrid

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 class HomeController < ApplicationController
 layout "before_login"
+	protect_from_forgery  :except=>:check_email_reply_and_save
 def index
 	redirect_to '/messages' if current_user
 end	
+
+def check_email_reply_and_save
+	
+end
 
 end
