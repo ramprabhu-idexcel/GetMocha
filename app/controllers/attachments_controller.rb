6574b945c6f8c3e683292a56e258c773e9f6bc97
Selva <c.selvakumar@sedin.co.in>
2011-03-22 17:25:09 +0530

    Drag and drop file upload, remove completed

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -5,25 +5,19 @@ class AttachmentsController < ApplicationController
 	end
 	
 	def create
-		session[:attaches_id] ||= ""
+		session[:attaches_id] ||= []
 		@attachment=Attachment.new(:uploaded_data => params["undefined"])
 		@attachment.save
 		p @attachment.id
-		session[:attaches_id] +="#{@attachment.id},"
+		session[:attaches_id] << @attachment.id
 		p session[:attaches_id]
 		#~ render :nothing=>true
 		render :json=>{:file=>@attachment.filename, :id=>@attachment.id}.to_json
 	end
 	def remove_attach
-		p session[:attaches_id] ||= ""
-		p params[:id]
-		@attach=Attachment.delete(params[:id])
-	p	session[:attaches_id].split(',')
-		p session[:attaches_id].split(',').delete('params[:id]')
-		p session[:attachess_id]
-		p"============="
-	#	render :nothing=>true
-		render :json=>params[:id].to_json
+	@attach=Attachment.delete(params[:id])
+	 session[:attaches_id].delete(params[:id].to_i)
+	render :json=>params[:id].to_json
 	end
 	
 	
