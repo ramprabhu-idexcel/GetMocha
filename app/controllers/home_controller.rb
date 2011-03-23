efe8311ef62b13d03768f41bec7cc0b383328736
Selva <c.selvakumar@sedin.co.in>
2011-03-23 16:04:12 +0530

    Added a method for callback url from sendgrid

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,10 +13,28 @@ def check_email_reply_and_save
 		 @dest_address=@dest_address[0]
      logger.info @dest_address
      if @dest_address.include?("#{APP_CONFIG[:project_email]}")
-       new_project_create_via_mail
-     elsif @dest_address.include?("#{APP_CONFIG[:message_email]}")
+        from_address=params[:from].to_s
+				user=User.find_by_email(from_address)
+				if user 
+					message=params[:html].to_s
+					name=params[:subject].to_s
+					project=Project.create(:user_id=>user.id, :name=>name, :is_public=>true)
+					params[:to].each do |mail|
+						if !mail.to_s.include?("p.rfmocha.com")
+							invite=Invitation.create(:email=>mail,:message=>message,:project_id=>project.id)
+							ProjectMailer.delay.invite_people(user,invite)
+						end
+					end
+					params[:cc].each do |mail|
+						if !mail.to_s.include?("p.rfmocha.com")
+							invite=Invitation.create(:email=>mail,:message=>message,:project_id=>project.id)
+							ProjectMailer.delay.invite_people(user,invite)
+						end
+					end
+				end
+			elsif @dest_address.include?("#{APP_CONFIG[:message_email]}")
        #~ new_message_create_via_mail
-     elsif @dest_address.downcase.include?("ctzm")
+			elsif @dest_address.downcase.include?("ctzm")
        #~ comment_for_message_via_mail(email)
      end
     end
