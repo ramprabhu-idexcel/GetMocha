44b44d2ab383b040e934682321cd043315a95733
Selva <c.selvakumar@sedin.co.in>
2011-03-23 10:34:18 +0530

    Host and smtp details added temp in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,7 @@ GetMocha::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => 'getmocha.com' }
   
    ActionMailer::Base.smtp_settings = {
     :address => "smtp.gmail.com",
