e314011ae8ab88d65a997ad2bfc93e89a6876a0e
senthilkumar <senthilkumar@sedin.co.in>
2011-03-23 11:57:45 +0530

    http authentication added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 skip_before_filter :verify_authenticity_token
   #~ protect_from_forgery  layout :change_layout
+  before_filter :http_authenticate
   before_filter :find_project
   layout :change_layout
   
@@ -29,6 +30,17 @@ skip_before_filter :verify_authenticity_token
     @project=Project.find_by_id(params[:project_id]) if params[:project_id]
     session[:project_name]=@project.name if @project
   end
+  
+ 
+  protected
+
+  def http_authenticate
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == "getmocha" && password == "m0cha345"
+    end
+    warden.custom_failure! if performed?
+  end
+
   
    
   
