44841d58e8f8e9de12348e06c15cfec587323081
Selva <c.selvakumar@sedin.co.in>
2011-03-24 15:50:16 +0530

    Change on message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -72,7 +72,7 @@ class Message < ActiveRecord::Base
   end
   
   def subscribed_user_names
-    subscribed_users.collect{|a| a.user.name}.sort
+    subscribed_users.collect{|a| a.user.name}.sort if !subscribed_users.blank?
   end
   
   def display_subscribed_users
