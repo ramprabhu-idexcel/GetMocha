362a1e4ec36d72206e836c0405dfbed194d76e2f
Selva <c.selvakumar@sedin.co.in>
2011-03-24 12:10:10 +0530

    added confirmation controller

---
+++ app/controllers/confirmation_controller.rb
@@ -0,0 +1,15 @@
+class ConfirmationController < Devise::ConfirmationsController
+	
+	def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+
+    if resource.errors.empty?
+      set_flash_message :notice, :confirmed
+			 redirect_to   "/sign_in"
+    else
+        render_with_scope :new
+			end
+			
+	end
+		
+end
