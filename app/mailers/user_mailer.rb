9f16116bbbc1e7e841f5ee1aa4d9a9f82c93144e
senthilkumar <senthilkumar@sedin.co.in>
2011-03-25 16:27:39 +0530

    sign changed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
   default :from => "mochabot@getmocha.com"
    def verify_secondary_email(email,code)
     @url=verify_secondary_email_url(:verification_code=>code)
-    mail(:to=>email, :subject=>"Verify Your Email Address on Mocha",:content_type="text/html")
+    mail(:to=>email, :subject=>"Verify Your Email Address on Mocha",:content_type=>"text/html")
     @content_type="text/html"
    end
    # Method added for postfix functionality
