1ee55c8a52599f34e7bf96c1ac46e5bf39e16491
Selva <c.selvakumar@sedin.co.in>
2011-03-25 12:49:59 +0530

    created the passwords controller

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,19 @@
+class PasswordsController < Devise::PasswordsController
+  layout "before_login"
+  prepend_before_filter :require_no_authentication
+  include Devise::Controllers::InternalHelpers
+  # PUT /resource/password
+  def update
+   self.resource = resource_class.reset_password_by_token(params[:user])
+      
+    if resource.errors.empty?
+      set_flash_message :notice, :updated
+      sign_in_and_redirect(resource_name, resource)
+    else
+      errors=[]
+      resource.errors.each_full{|msg| errors<< msg } 
+      render :json=>{:failure=>errors.join("\n")}.to_json
+    end 
+				       
+    end   
+end
