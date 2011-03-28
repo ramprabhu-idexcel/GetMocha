c1570066a688338b1aa5d93020cc5ef2be489aa7
Selva <c.selvakumar@sedin.co.in>
2011-03-28 19:27:35 +0530

    modified in attachment.rb,message.rb

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,16 +2,14 @@ require 'aws/s3'
 class Attachment < ActiveRecord::Base
   include AWS::S3
   belongs_to :attachable, :polymorphic => true	
+
   #~ has_attachment :content_type => ['application/pdf', 'application/msword', 'text/plain']
   named_scope :recent_attachments, :conditions=>['attachable_id IS NULL AND parent_id IS NULL']
     if RAILS_ENV=="development"
   has_attachment :size => 1.megabyte..2.megabytes,:thumbnails => {:big => "461x461>", :small => "21x20",:profile=>"69x69",:message=>"75x75"},:storage => :file_system, :path_prefix => 'public/attachments',  :processor => 'Rmagick'
   else
   has_attachment :size => 1.megabyte..2.megabytes,:thumbnails => {:big => "461x461>", :small => "21x20",:profile=>"69x69",:message=>"75x75"},:storage => :s3, :path_prefix => 'public/attachments',  :processor => 'Rmagick'
-<<<<<<< HEAD
-=======
 end
->>>>>>> b043fea05d501ae8b2f2320fb92e9ff121c85107
   #~ named_scope :recent_attachments, :conditions=>['attachable_id IS NULL']
   #~ named_scope :user_attachments, :conditions=>['attachable_id = ?',self.user.id], :limit=> 1
   #~ after_save :resize_image_for_thumbnail
@@ -39,38 +37,25 @@ end
             else
               logger.info(img_part)
               img_part = img.crop(Magick::CenterGravity,size,size)
-              end
-            img_part=img_part.resize(file.image_width,file.image_width)
-<<<<<<< HEAD
-            file_path="#{Rails.root}/public/#{file.filename}"
-            img_part.write(file_path)
             end
-          end
-          end
-          end
-          end
-    def create_event
-=======
+            img_part=img_part.resize(file.image_width,file.image_width)
             if RAILS_ENV=="development"
               img_part.write(save_path)
             else
               file_path="#{Rails.root}/public/#{file.filename}"
               img_part.write(file_path)
-              
-             end 
-          end 
-        end 
+            end
+          end
+        end
       end
     end
   end
-  
 	def create_event
->>>>>>> b043fea05d501ae8b2f2320fb92e9ff121c85107
 		if self.project_id
 			Event.create_event(self,self.project_id,nil)
-      end
-      end
-     def image_width
+		end
+	end
+  def image_width
     case self.thumbnail
       when "small"
         75
@@ -81,11 +66,10 @@ end
       when "big"
       461
     end
-    end
-    def find_thumbnail(name)
+  end
+  def find_thumbnail(name)
     image=Attachment.find_by_parent_id_and_thumbnail(id,name)
   end
-
 end
 
 
