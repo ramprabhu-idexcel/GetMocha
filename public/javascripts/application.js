4983edc6cd5453786c94031558f5d14ef9fa374e
kiruba <kiruba.v@railsbuddies.com>
2011-03-29 13:17:29 +0530

    modified application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -851,9 +851,9 @@ else
         a=data.search("try")
 	alert(a);
         if(a!=0){
+         $('.add-item-modal').hide();
          document.getElementById('messages_projects_list').innerHTML=data;
-        $('.add-item-modal').hide();
-       }
+        }
       },
       failure: function(){
         alert("Error");
