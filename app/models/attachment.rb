8a10cd6b98004c4c68c6402f7b2c968d85c0983c
senthilkumar <senthilkumar@sedin.co.in>
2011-03-29 14:36:36 +0530

    rails env condition changed

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -6,25 +6,25 @@ class Attachment < ActiveRecord::Base
   belongs_to :attachable, :polymorphic => true
   #~ has_attachment :content_type => ['application/pdf', 'application/msword', 'text/plain']
   named_scope :recent_attachments, :conditions=>['attachable_id IS NULL AND parent_id IS NULL']
-    if RAILS_ENV=="development"
-  has_attachment :size => 1.megabyte..2.megabytes,:thumbnails => {:big => "461x461>", :small => "21x20",:profile=>"69x69",:message=>"75x75"},:storage => :file_system, :path_prefix => 'public/attachments',  :processor => 'Rmagick'
+  if Rails.env.development?
+    has_attachment :size => 1.megabyte..2.megabytes,:thumbnails => {:big => "461x461>", :small => "21x20",:profile=>"69x69",:message=>"75x75"},:storage => :file_system, :path_prefix => 'public/attachments',  :processor => 'Rmagick'
   else
-  has_attachment :size => 1.megabyte..2.megabytes,:thumbnails => {:big => "461x461>", :small => "21x20",:profile=>"69x69",:message=>"75x75"},:storage => :s3, :path_prefix => 'public/attachments',  :processor => 'Rmagick'
-end
+    has_attachment :size => 1.megabyte..2.megabytes,:thumbnails => {:big => "461x461>", :small => "21x20",:profile=>"69x69",:message=>"75x75"},:storage => :s3, :path_prefix => 'public/attachments',  :processor => 'Rmagick'
+  end
 
   #~ named_scope :recent_attachments, :conditions=>['attachable_id IS NULL']
   #~ named_scope :user_attachments, :conditions=>['attachable_id = ?',self.user.id], :limit=> 1
   #~ after_save :resize_image_for_thumbnail
   def resize_image_for_thumbnail
     #~ p self.thumbnails
-      if self.content_type.split('/')[0]=="image"
-        if self.thumbnail.nil?
+    if self.content_type.split('/')[0]=="image"
+      if self.thumbnail.nil?
         self.thumbnails.each do |file|
-          fixed_width=200
+        fixed_width=200
           unless file.thumbnail=="big"
             width=file.parent.width
             height=file.parent.height
-            if RAILS_ENV=="development"
+            if Rails.env.development?
               full_path = File.join(Rails.root, 'public/', file.parent.public_filename)
               save_path = File.join(Rails.root, 'public/', file.public_filename)
             else
@@ -39,25 +39,25 @@ end
             else
               logger.info(img_part)
               img_part = img.crop(Magick::CenterGravity,size,size)
-              end
+            end
             img_part=img_part.resize(file.image_width,file.image_width)
             file_path="#{Rails.root}/public/#{file.filename}"
             img_part.write(file_path)
-            end
           end
-          end
-          end
-          end
-    def create_event
-            if RAILS_ENV=="development"
-              img_part.write(save_path)
-            else
-              file_path="#{Rails.root}/public/#{file.filename}"
-              img_part.write(file_path)
-            end
-          end
-     def image_width
-   case self.thumbnail
+        end
+      end
+    end
+  end
+  def create_event
+    if Rails.env.development?
+      img_part.write(save_path)
+    else
+      file_path="#{Rails.root}/public/#{file.filename}"
+      img_part.write(file_path)
+    end
+  end
+  def image_width
+    case self.thumbnail
       when "small"
         75
       when "message"
@@ -67,8 +67,8 @@ end
       when "big"
       461
     end
-    end
-    def find_thumbnail(name)
+  end
+  def find_thumbnail(name)
     image=Attachment.find_by_parent_id_and_thumbnail(id,name)
   end
 end
