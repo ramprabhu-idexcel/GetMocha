bc5fb1141f94a1d3fd293c09177917be907f01ff
Selva <c.selvakumar@sedin.co.in>
2011-03-31 11:31:11 +0530

    Issue on reset password and enter button shortcut fixed

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -3,6 +3,17 @@ class PasswordsController < Devise::PasswordsController
   prepend_before_filter :require_no_authentication
   include Devise::Controllers::InternalHelpers
   # PUT /resource/password
+    def edit
+    self.resource = resource_class.new
+        a=User.find_by_reset_password_token(params[:reset_password_token])
+    resource.reset_password_token = params[:reset_password_token]
+
+    if !a
+      redirect_to '/signin'
+  else
+    render_with_scope :edit
+ end
+  end
   def update
    self.resource = resource_class.reset_password_by_token(params[:user])
      if resource.errors.empty?
