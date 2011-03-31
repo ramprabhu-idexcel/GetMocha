276ba3e47ec469fa414dbfa01134db7b820cd542
kiruba <kiruba.v@railsbuddies.com>
2011-03-31 19:24:35 +0530

    after changes in js

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -5,28 +5,25 @@ class PasswordsController < Devise::PasswordsController
   # PUT /resource/password
    def create
          errors=[]
-     
     self.resource = resource_class.send_reset_password_instructions(params[resource_name])
     if resource.errors.empty?
       set_flash_message :notice, :send_instructions
       redirect_to new_session_path(resource_name)
     else
-      
-     puts resource.errors
-    resource.errors.each_full{|msg| errors<<msg }
+      puts resource.errors
+      resource.errors.each_full{|msg| errors<<msg }
       render :json=>{:failure=>errors.join("\n")}.to_json
     end
-    end
-    def edit
+  end
+  def edit
     self.resource = resource_class.new
-        a=User.find_by_reset_password_token(params[:reset_password_token])
+    a=User.find_by_reset_password_token(params[:reset_password_token])
     resource.reset_password_token = params[:reset_password_token]
-
     if !a
       redirect_to '/signin'
-  else
-    render_with_scope :edit
- end
+    else
+      render_with_scope :edit
+    end
   end
   def update
    self.resource = resource_class.reset_password_by_token(params[:user])
