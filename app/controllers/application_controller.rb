3b6558817b06b71e60077f0d7b00a262e696a8e5
Selva <c.selvakumar@sedin.co.in>
2011-04-01 16:12:40 +0530

    application cont

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 skip_before_filter :verify_authenticity_token
 #~ protect_from_forgery  layout :change_layout
 before_filter :http_authenticate, :except=>['']
-before_filter :from_email_id,:only=>['new_project_via_email','message_create_via_email','reply_to_message_via_email']
+before_filter :check_from_address_email,:only=>['new_project_via_email','message_create_via_email','reply_to_message_via_email']
 before_filter :find_project
 layout :change_layout
   def change_layout
@@ -219,17 +219,18 @@ layout :change_layout
     warden.custom_failure! if performed?
   end
 	
-	def check_from_address_email(from_address)
+	def check_from_address_email
+		@from_address=(params[:from].to_s)
 		logger.info "Start"
-			if(from_address.include?('<'))
-					from_address=from_address.split('<')
-					from_address=from_address[1].split('>')
-					from_address=from_address[0]
+			if(@from_address.include?('<'))
+					@from_address=@from_address.split('<')
+					@from_address=@from_address[1].split('>')
+					@from_address=@from_address[0]
 				end
-				logger.info from_address
-			return from_address
+				logger.info @from_address
+			
 		end	
-	def from_email_id
-	@from_address=check_from_address_email(params[:from].to_s)
-  end		
+	#~ def from_email_id
+	#~ @from_address=(params[:from].to_s)
+  #~ end		
 end
