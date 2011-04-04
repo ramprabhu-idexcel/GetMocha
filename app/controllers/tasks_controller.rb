932aae18b3cd6c8610bc49909907fbf9bfd2db52
senthilkumar <senthilkumar@sedin.co.in>
2011-04-04 12:34:09 +0530

    removed the session variable temporarily

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,41 +3,28 @@ class TasksController < ApplicationController
 #  before_filter :find_activity,:only=>['subscribe','star_task','show','unsubscribe','destroy','project_task_comment']
 	layout 'application', :except=>['new']
 	def index
-		session[:project_name][]=nil
-		#~ session[:project_selected]=nil
+		#~ session[:project_name][]=nil
 		@projects=current_user.user_active_projects
 	end
-		def new
-			
+  def new
 		session[:attaches_id]=nil
 		attachs=Attachment.recent_attachments
 		attachs.each do |attach|
-		Attachment.delete(attach)
+      Attachment.delete(attach)
 		end
-		#~ if session[:project_name]
-			#~ project=Project.find_by_name(session[:project_name])
-			#~ @users=project.users
-		#~ else
-		  @users=current_user.my_contacts
-		#~ end
-		p @user.inspect
+    @users=current_user.my_contacts
 		@projects=Project.find(:all,:select=>{[:name],[:id]},:conditions=>['project_users.user_id=?',current_user.id],:include=>:project_users)
-		p @projects
 		@user_emails=[]
 		@t_list=[]
 		@project_names=[]
 		if @users
 		  @users.each do |f|
-		  @user_emails<<"#{f.email}"
+        @user_emails<<"#{f.email}"
 		  end
 		end
 	  @projects.each do |project|
-		@project_names<<"#{project.name}"
-	end
-	#~ @tlist=@projects.task_lists.find(:all)
-	#~ @tlist.each do |tl|
-		#~ p @t_list<<"#{tl.name}"
-		  #~ end
+      @project_names<<"#{project.name}"
+    end
 	  render :partial=>'new'
 	end
 	def create
