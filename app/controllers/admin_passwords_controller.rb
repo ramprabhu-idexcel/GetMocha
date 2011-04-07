355ecf6adc25c2f428671798551a70ff5a3ea317
kiruba <kiruba.v@railsbuddies.com>
2011-04-07 09:33:39 +0530

    admin added

---
+++ app/controllers/admin_passwords_controller.rb
@@ -0,0 +1,43 @@
+class AdminPasswordsController < Devise::PasswordsController
+  prepend_before_filter :require_no_authentication
+  include Devise::Controllers::InternalHelpers
+  # PUT /resource/password
+   def create
+         errors=[]
+         p "-------------------"
+         p params
+         self.resource = resource_class.send_reset_password_instructions(params[:resource_name])
+         p"---------------------"
+         p resource.inspect
+         p"-------------"
+	    #self.resource = resource_class.send_reset_password_instructions(params[resource_name])
+    if resource.errors.empty?
+      set_flash_message :notice, :send_instructions
+      redirect_to new_session_path(resource_name)
+    else
+      puts resource.errors
+      resource.errors.each_full{|msg| errors<<msg }
+      render :json=>{:failure=>errors.join("\n")}.to_json
+    end
+  end
+  def edit
+    self.resource = resource_class.new
+    a=User.find_by_reset_password_token(params[:reset_password_token])
+    resource.reset_password_token = params[:reset_password_token]
+    if !a
+      redirect_to '/signin'
+    else
+      render_with_scope :edit
+    end
+  end
+  def update
+   self.resource = resource_class.reset_password_by_token(params[:user])
+     if resource.errors.empty?
+      set_flash_message :notice, :updated
+      sign_in_and_redirect(resource_name, resource)
+    else
+      resource.errors.each_full{|msg| errors<< msg }
+      render :json=>{:failure=>errors.join("\n")}.to_json
+    end
+    end
+   end
