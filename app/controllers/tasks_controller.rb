14101228b59509766d638c1c150f15a9386c0c65
kiruba <kiruba.v@railsbuddies.com>
2011-04-12 16:43:27 +0530

    email templates

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -69,7 +69,7 @@ class TasksController < ApplicationController
 			else
 			if !params[:task][:notify].blank?
 				#~ errors<<"Please enter To_email address"
-				if !params[:task][:recipient].match(/([a-z0-9_.-]+)@([a-z0-9-]+)\.([a-z.]+)/i)
+				if !params[:task][:notify].match(/([a-z0-9_.-]+)@([a-z0-9-]+)\.([a-z.]+)/i)
 					errors<<"Please enter valid notify email"
 					end
 		    end
