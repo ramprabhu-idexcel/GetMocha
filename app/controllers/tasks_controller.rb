86bc7bb95e74bd200e7ed7c58c55a31170528217
senthilkumar <senthilkumar@sedin.co.in>
2011-04-12 20:30:13 +0530

    alert error message displayed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,22 +49,22 @@ class TasksController < ApplicationController
 		  end
 		else
       if params[:task][:tasklist].blank?
-			  errors<< "Please Enter the tasklist name"
-      end
+      errors<< "Please Enter the tasklist name"
       if !params[:task][:recipient].blank?
-					#~ errors<<"Please enter To_email address"
-        if params[:task][:recipient].match(/\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i).nil?
-          errors<<"Please enter valid email for assign"
+      #~ errors<<"Please enter To_email address"
+      if !params[:task][:recipient].match(/([a-z0-9_.-]+)@([a-z0-9-]+)\.([a-z.]+)/i)
+        errors<<"Please enter valid email for assign"
         else
           u_email=[]
           @project.users.each do |user|
             u_email<<user.email
           end
-          e=u_email.include?(params[:task][:recipient])
+          e=u_email.indexOf(params[:task][:recipient])
           if !e
             errors<<"Please select existing user only"
           end
-	      end
+        end
+      end
 			else
 			if !params[:task][:notify].blank?
 				#~ errors<<"Please enter To_email address"
