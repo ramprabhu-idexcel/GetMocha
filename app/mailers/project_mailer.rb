5ea4c3c96ee0e4e167a5a6ae859b5ed1d40ca076
Selva <c.selvakumar@sedin.co.in>
2011-04-12 16:47:15 +0530

    issue on task notification fixed

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -122,8 +122,8 @@ class ProjectMailer < ActionMailer::Base
       @people<<info_activity_user.full_name<<"," if activity.user
      end
    end
-   t=self.activities.find_by_user_id(@existing_user.id) if @existing_user
-   if t.is_assigned==true
+   t=task.activities.find_by_user_id(@existing_user.id) if @existing_user
+   if t && t.is_assigned==true
     mail(:from=>"#{from}", :to=>"#{to_user}", :reply_to=>"ctzt#{task.id}@#{APP_CONFIG[:reply_email]}", :subject=>"#{user.first_name} assigned a new task to #{to_user}",:content_type=>"text/html")
     @content_type="text/html"
     else
