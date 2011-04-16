e2b763f0d36ae2192125034769854b521d7bd0cd
kiruba <kiruba.v@railsbuddies.com>
2011-04-16 17:12:18 +0530

    chat controller for invite

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,8 +2,16 @@ class ChatsController < ApplicationController
   before_filter :authenticate_user!
   def index
     @projects=Project.user_active_projects(current_user.id)
+    @user_emails=new
   end
-  def create
+  def new
+    @users=User.all_users
+	  @user_emails=[]
+	  @users.each do |f|
+			@user_emails<<"#{f.email}"
+      end
+		end
+	  def create
     send_to_clients ["message", current_user.full_name, params[:chat][:message]]
     render :nothing=>true
   end
