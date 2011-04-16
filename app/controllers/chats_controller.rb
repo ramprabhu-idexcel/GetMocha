b6c8ca296306f74bcda4799e054e66026456115a
senthilkumar <senthilkumar@sedin.co.in>
2011-04-16 15:05:27 +0530

    basic methods added

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,4 +3,12 @@ class ChatsController < ApplicationController
   def index
     @projects=Project.user_active_projects(current_user.id)
   end
+  def create
+    send_to_clients ["message", current_user.full_name, params[:chat][:message]]
+    render :nothing=>true
+  end
+  private
+  def send_to_clients(data)	
+    Socky.send(data.collect{|d| CGI.escapeHTML(d)}.to_json,:to=>{:channels=>1})
+	end
 end
