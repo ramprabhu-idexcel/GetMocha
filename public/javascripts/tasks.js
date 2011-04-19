033740ec5fbaa06749b17353fd70c2317530bdab
Selva <c.selvakumar@sedin.co.in>
2011-04-19 11:33:01 +0530

    changed done in tasks.js

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -568,7 +568,7 @@ return false;
       items.push('<div class="clear-fix"></div>')
       items.push('</div>');
     }
-    items.push('<p class="subscribers">'+task.subscribe+'<span id="all_subscribed" style="display:none;">'+task.all_subscribed+'</span><a id="subscribe_task" class="task-subscribe" href="#">unsubscribe</a></p>');
+    items.push('<p class="subscribers">'+task.subscribe+'<span id="all_subscribed" style="display:none;">'+task.all_subscribed+'</span><a id="subscribe_task" class="task-subscribe" href="#"> Unsubscribe</a></p>');
     items.push('<span id="pk:'+task.project_id+'" class="pl_tk" style="display:none">Show</span>');
     items.push('</div>');
     //comments
