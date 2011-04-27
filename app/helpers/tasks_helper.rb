bc911e64874de01daa2dad1f9f1b7b8e458a1999
senthilkumar <senthilkumar@sedin.co.in>
2011-04-27 15:40:18 +0530

    method to find the task count changed

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -11,7 +11,7 @@ module TasksHelper
     link_to form_content(count,"All Tasks"),"#all_tasks",:class=>"all-tasks open",:id=>"all_tasks"
   end
   def link_to_my_tasks
-    count=current_user.my_tasks(nil).count
+    count=current_user.find_my_tasks(nil).count
     link_to form_content(count,"My Tasks"),"#my_tasks",:class=>"my-tasks", :id=>"my_tasks"
   end
   def link_to_starred_tasks
@@ -22,14 +22,14 @@ module TasksHelper
     count=current_user.completed_tasks(nil,nil).count
     if(count>0)
       link_to form_content(count,"Completed"),"#completed_tasks",:class=>"completed",:id=>"completed_tasks"
-     else
-     link_to form_content(count,"Completed"),"#completed_tasks",:class=>"completed",:style=>"display:none;",:id=>"completed_tasks"
-   end
-   end
+    else
+      link_to form_content(count,"Completed"),"#completed_tasks",:class=>"completed",:style=>"display:none;",:id=>"completed_tasks"
+    end
+  end
   def project_tab(project)
-      content_tag(:div,'',:class=>'project',:id=>"tpi#{project.id}") do
-        content_tag(:span,'',:class=>'icon')+content_tag(:span,content_tag(:span,'',:class=>'icon')+project.name,:class=>'project-title')+task_list_project(project.task_lists)+content_tag(:div,'',:class=>'clear-fix')
-      end
+    content_tag(:div,'',:class=>'project',:id=>"tpi#{project.id}") do
+      content_tag(:span,'',:class=>'icon')+content_tag(:span,content_tag(:span,'',:class=>'icon')+project.name,:class=>'project-title')+task_list_project(project.task_lists)+content_tag(:div,'',:class=>'clear-fix')
+    end
   end
   def task_list_project(task_lists)
     content_tag :ul do
